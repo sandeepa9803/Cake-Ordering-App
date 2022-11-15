4b63e7969006ff1a2ad1a82b39241f8403c68fa2
mihiran03 <it20297886@my.sliit.lk>
2022-11-16 00:13:09 +0530

    Added update and delete pages including all the drawable, fonts, layouts and java files

---
+++ app/src/main/java/com/example/homenew/update.java
@@ -0,0 +1,47 @@
+package com.example.homenew;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class update extends AppCompatActivity {
+
+    NotificationManagerCompat notificationManagerCompat;
+    Notification notification;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("myCh", "My Channel", NotificationManager.IMPORTANCE_DEFAULT);
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "myCh")
+                .setSmallIcon(android.R.drawable.stat_notify_sync)
+                .setContentTitle("First Notification")
+                .setSettingsText("This is body of message");
+
+        notification = builder.build();
+
+        notificationManagerCompat = NotificationManagerCompat.from(this);
+    }
+    public void push(View view){
+        notificationManagerCompat.notify(1,notification);
+    }
+
+}
